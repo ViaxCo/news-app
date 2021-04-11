5473d56f8b2be041a42118f4a74e9fdd5748c75f
Victor Ajibade <ajibadevic@gmail.com>
2021-04-11 21:59:37 +0100

    Add error catching for API call

--- mobx/fetchNewsAndSaveToDB.ts
+++ mobx/fetchNewsAndSaveToDB.ts
@@ -8,32 +8,37 @@ const fetchNewsAndSaveToDB = async () => {
   const firestore = firebase.firestore();
   const articlesRef = firestore.collection("articles");
 
-  // Fetch news from Nigeria
-  const res = await axios.get(
-    `https://api.currentsapi.services/v1/search?apiKey=${process.env.API_KEY}&country=NG`
-  );
-  const news = res.data.news as ArticleType[];
-  // Filter out articles with no image and long titles
-  const filteredNews = news.filter(
-    article => article.image !== "None" && article.title.length <= 140
-  );
-  filteredNews.forEach(async article => {
-    const slug = kebabCase(article.title);
-    try {
-      // Check if the article already exists in the db
-      const doc = await articlesRef.doc(slug).get();
-      // If it doesn't, save it
-      if (!doc.exists) {
-        await articlesRef.doc(slug).set({
-          ...article,
-          id: slug,
-          comments: [],
-        });
+  try {
+    // Fetch news from Nigeria
+    const res = await axios.get(
+      `https://api.currentsapi.services/v1/search?apiKey=${process.env.API_KEY}&country=NG`
+    );
+    const news = res.data.news as ArticleType[];
+    // Filter out articles with no image and long titles
+    const filteredNews = news.filter(
+      article => article.image !== "None" && article.title.length <= 140
+    );
+    filteredNews.forEach(async article => {
+      const slug = kebabCase(article.title);
+      try {
+        // Check if the article already exists in the db
+        const doc = await articlesRef.doc(slug).get();
+        // If it doesn't, save it
+        if (!doc.exists) {
+          await articlesRef.doc(slug).set({
+            ...article,
+            id: slug,
+            comments: [],
+          });
+        }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
-    }
-  });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
   // Fetch all the articles from db and save to state
   const querySnapshot = await articlesRef.orderBy("published", "desc").get();
   return querySnapshot;
