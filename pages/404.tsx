39f10c2cb257d10598c3d1a1a59cce3a631d2111
Victor Ajibade <ajibadevic@gmail.com>
2023-05-18 12:54:35 +0100

    Migrate to App Router: Replace Not Found page

--- pages/404.tsx
+++
@@ -1,7 +0,0 @@
-import NotFound from "@/components/NotFound";
-
-const Custom404 = () => {
-  return <NotFound param="page" />;
-};
-
-export default Custom404;
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,12 @@
-type Props = {
-  param: string;
-};
-
-const NotFound = ({ param }: Props) => {
+export default function NotFound() {
   return (
     <div className="my-auto flex items-center justify-center">
       <div className="flex items-center space-x-4">
         <h2 className="font-semibold text-2xl border-r-2 border-solid border-black p-4 py-2">
           404
         </h2>
-        <h3 className="text-base font-normal">This {param} could not be found</h3>
+        <h3 className="text-base font-normal">This page could not be found</h3>
       </div>
     </div>
   );
-};
-
-export default NotFound;
+}
