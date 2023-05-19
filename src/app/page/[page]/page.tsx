aab2c9a1316e69077a645000485be3125e9c786c
Victor Ajibade <ajibadevic@gmail.com>
2023-05-19 12:12:51 +0100

    Update page error catching

--- src/app/page/[page]/page.tsx
+++ src/app/page/[page]/page.tsx
@@ -1,6 +1,7 @@
 import type { Article, ArticleFromApi } from "@/components/ArticleCard";
 import Articles from "@/components/Articles";
 import Pagination from "@/components/Pagination";
+import path from "path";
 import { getPlaiceholder } from "plaiceholder";
 
 const totalPages = 25;
@@ -28,20 +29,41 @@ const getArticles = async (page: string) => {
       articles.map(async article => {
         try {
           const { base64 } = await getPlaiceholder(
-            article.image === "None" ? "/fallback.jpg" : article.image
+            article.image === "None" ? "/fallback.jpg" : article.image,
+            {
+              // Use path.resolve to allow the serverless function include the contents of the public folder
+              dir: path.resolve(process.cwd() + "/public"),
+            }
           );
           return {
             src: article.image,
             base64,
           };
         } catch (error) {
-          // Incase plaiceholder fails to transform the image, return the placeholder based on the fallback image
-          const { base64 } = await getPlaiceholder("/fallback.jpg");
-          console.error({ error, image: article.image, title: article.title, page });
-          return {
-            src: article.image,
-            base64,
-          };
+          console.error({
+            error,
+            image: article.image,
+            title: article.title,
+            page,
+            type: "getPlaiceholder error",
+          });
+          try {
+            // Incase plaiceholder fails to transform the image, return the placeholder based on the fallback image
+            const { base64 } = await getPlaiceholder("/fallback.jpg", {
+              // Use path.resolve to allow the serverless function include the contents of the public folder
+              dir: path.resolve(process.cwd() + "/public"),
+            });
+            return {
+              src: article.image,
+              base64,
+            };
+          } catch (error) {
+            console.error({ error, type: "getPlaiceholder fallback error" });
+            return {
+              src: article.image,
+              base64: "",
+            };
+          }
         }
       })
     );
@@ -54,7 +76,7 @@ const getArticles = async (page: string) => {
     return updatedArticles;
   } catch (error) {
     // TODO: Test the error by modifying any of the above `await's`
-    console.error(error);
+    console.error({ error, type: "Either fetch or Promise.all failed" });
     return [];
   }
 };
