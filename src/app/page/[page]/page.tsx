5af26d55723b9e7259bab1bde4d429b3a8807814
Victor Ajibade <ajibadevic@gmail.com>
2023-05-19 20:56:53 +0100

    Extract getArticles function into file

--- src/app/page/[page]/page.tsx
+++ src/app/page/[page]/page.tsx
@@ -1,8 +1,6 @@
-import type { Article } from "@/components/ArticleCard";
 import Articles from "@/components/Articles";
 import Pagination from "@/components/Pagination";
-import type { IGetPlaiceholderReturn } from "plaiceholder";
-import { getPlaiceholder } from "plaiceholder";
+import getArticles from "@/utils/getArticles";
 
 const totalPages = 25;
 
@@ -11,53 +9,6 @@ export const dynamicParams = false;
 export const generateStaticParams = async () => {
   const pages = Array.from({ length: totalPages }, (_, i) => (i + 1).toString());
   return pages.map(page => ({ page }));
-};
-
-const getArticles = async (page: string) => {
-  try {
-    // Fetch news from Nigeria
-    const res = await fetch(
-      `https://api.currentsapi.services/v1/search?apiKey=${process.env.API_KEY}&country=NG&page_number=${page}&page_size=20&domain_not=theguardian.com`,
-      // revalidate every 3 hours
-      { next: { revalidate: 10800 } }
-    );
-    const data = await res.json();
-    const articles = data.news as Article[];
-
-    // base64 placeholder of the fallback image in /public
-    const fallbackBase64 =
-      "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAAECAIAAAAmkwkpAAAACXBIWXMAAAsTAAALEwEAmpwYAAAANElEQVR4nB3BoREAMAgDQAwDMAtTMWh8ND4+HtO7/gcAfwDCdlVl5t2FpJnpbttBcnclkXxmJSGykN3v2wAAAABJRU5ErkJggg==";
-
-    // Transform images to use a placeholder image
-    const imagesProps = await Promise.allSettled(
-      articles.map(article =>
-        // Return the placholder promise if it resolves within 5 seconds, if not reject
-        Promise.race([getPlaiceholder(article.image), rejectPromiseAfterDelay(5000)])
-      )
-    );
-
-    const articlesWithPlaceholder: Article[] = articles.map((article, index) => {
-      const item = imagesProps[index];
-      if (item.status === "fulfilled") {
-        return { ...article, base64: item.value.base64 };
-      } else {
-        console.log({
-          error: item.reason,
-          title: article.title,
-          image: article.image,
-          type: "getPlaiceholder error",
-          page,
-        });
-        return { ...article, base64: fallbackBase64 };
-      }
-    });
-
-    return articlesWithPlaceholder;
-  } catch (error) {
-    // TODO: Test the error by modifying any of the above `await's`
-    console.error({ error, type: "Either fetch or Promise.all failed" });
-    return [];
-  }
 };
 
 type Props = {
@@ -77,8 +28,3 @@ const Page = async ({ params }: Props) => {
 };
 
 export default Page;
-
-const rejectPromiseAfterDelay = (ms: number) =>
-  new Promise<IGetPlaiceholderReturn>((_, reject) => {
-    setTimeout(reject, ms, new Error("timeout"));
-  });
