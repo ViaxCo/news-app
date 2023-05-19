5e28179dbebe0c25316adbac655b0d582edccb32
Victor Ajibade <ajibadevic@gmail.com>
2023-05-19 20:55:48 +0100

    Extraact page layout

--- src/app/page/[page]/page.tsx
+++ src/app/page/[page]/page.tsx
@@ -69,14 +69,10 @@ const Page = async ({ params }: Props) => {
   const articles = await getArticles(page);
 
   return (
-    <div className="flex flex-col flex-1">
-      <div className="border-b-2 border-solid border-black flex items-center justify-between">
-        <h2 className="ml-2 text-2xl sm:text-3xl font-semibold">Articles</h2>
-        <p className="mr-2 text-sm text-grey">Page {page}</p>
-      </div>
+    <>
       <Articles articles={articles} />
       <Pagination currentPage={+page} totalPages={totalPages} />
-    </div>
+    </>
   );
 };
 
